48a33fec1b1994f1038a0eb9197cc40ba30c0d8d
rygar08 <ryanlayton08@gmail.com>
2024-04-13 16:55:48 +1000

    estate model

---
+++ estate/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "estate",
+
+    'summary': """
+        
+    """,
+
+    'description': """
+         
+    """,
+
+    'author': "Odoo",
+    'website': "https://www.odoo.com",
+
+    'category': 'Tutorials/Estate',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': [],
+    'application': True,
+    'installable': True,
+    'data': [
+
+    ],
+    'license': 'AGPL-3'
+}
