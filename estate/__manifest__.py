cdc771ebe558c44427c0180f70da3981047c715c
ryan <ryanlayton08@gmail.com>
2024-04-17 20:10:55 +1000

    menu

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -17,11 +17,12 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': [],
+    # 'depends': ['base_setup', 'onboarding', 'product', 'analytic', 'portal', 'digest'],
     'application': True,
     'installable': True,
     'data': [
-
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
     ],
     'license': 'AGPL-3'
 }
